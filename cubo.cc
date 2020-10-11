d0b4b86910ac789b83939351f2d368b301f7c1bc
Sergio García <32015541+Olasergiolas@users.noreply.github.com>
2020-10-11 12:18:18 +0200

    Caras del cubo arregladas

--- cubo.cc
+++ cubo.cc
@@ -16,31 +16,31 @@ Cubo::Cubo(float lado)
    //  de las agujas del reloj, cuando esa cara se observa desde el exterior del cubo)
 
     //Inicializamos tabla de vértices
-    Tupla3f v0(lado, -lado, lado);
-    Tupla3f v1(lado, lado, lado);
-    Tupla3f v2(-lado, lado, lado);
-    Tupla3f v3(-lado, -lado, lado);
-    Tupla3f v4(lado, -lado, -lado);
-    Tupla3f v5(lado, lado, -lado);
-    Tupla3f v6(-lado, lado, -lado);
-    Tupla3f v7(-lado, -lado, -lado);
+    Tupla3f v0(-lado, -lado, lado);
+    Tupla3f v1(lado, -lado, lado);
+    Tupla3f v2(lado, lado, lado);
+    Tupla3f v3(-lado, lado, lado);
+    Tupla3f v4(-lado, -lado, -lado);
+    Tupla3f v5(lado, -lado, -lado);
+    Tupla3f v6(lado, lado, -lado);
+    Tupla3f v7(-lado, lado, -lado);
 
     std::vector<Tupla3f> auxV{v0, v1, v2, v3, v4, v5, v6, v7};
     v = auxV;
 
     //Inicializamos tabla de triángulos
-    Tupla3i c0(0, 1, 3);
-    Tupla3i c1(1, 2, 3);
-    Tupla3i c2(4, 5, 0);
-    Tupla3i c3(5, 1, 0);
-    Tupla3i c4(7, 4, 5);
-    Tupla3i c5(7, 5, 6);
-    Tupla3i c6(7, 6, 3);
-    Tupla3i c7(6, 2, 3);
-    Tupla3i c8(1, 5, 2);
-    Tupla3i c9(5, 6, 2);
-    Tupla3i c10(0, 4, 3);
-    Tupla3i c11(4, 7, 3);
+    Tupla3i c0(0, 1, 2);
+    Tupla3i c1(2, 3, 0);
+    Tupla3i c2(7, 6, 5);
+    Tupla3i c3(5, 4, 7);
+    Tupla3i c4(1, 5, 6);
+    Tupla3i c5(6, 2, 1);
+    Tupla3i c6(4, 0, 3);
+    Tupla3i c7(3, 7, 4);
+    Tupla3i c8(3, 2, 6);
+    Tupla3i c9(6, 7, 3);
+    Tupla3i c10(4, 5, 1);
+    Tupla3i c11(1, 0, 4);
 
     std::vector<Tupla3i> auxC{c0, c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11};
     f = auxC;
