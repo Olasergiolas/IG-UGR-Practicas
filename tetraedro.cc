cd2f7e8bf5fee716dcc6799bc9651e1637621876
Sergio García <Olasergiolas@gmail.com>
2020-10-23 12:18:27 +0200

    Modified object's coordinates

--- tetraedro.cc
+++ tetraedro.cc
@@ -8,17 +8,17 @@ Tetraedro::Tetraedro()
 
    // inicializar la tabla de vértices
 
-    
+
    // inicializar la tabla de caras o triángulos:
    // (es importante en cada cara ordenar los vértices en sentido contrario
    //  de las agujas del reloj, cuando esa cara se observa desde el exterior del cubo)
 
     float h = 100.0;
 
-    Tupla3f v0(0.0, -(h/2), (2*h)/3);
-    Tupla3f v1(0.5773*h, -(h/2), -(h/3));
-    Tupla3f v2(-(0.5773*h), -(h/2), -(h/3));
-    Tupla3f v3(0, h/2, 0);
+    Tupla3f v0(0.0, 0, (2*h)/3);
+    Tupla3f v1(0.5773*h, 0, -(h/3));
+    Tupla3f v2(-(0.5773*h), 0, -(h/3));
+    Tupla3f v3(0, h, 0);
 
     std::vector<Tupla3f> auxV{v0, v1, v2, v3};
     v = auxV;
